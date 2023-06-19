ed2303081fdb1f6ed9e9e7f352b64c20502e0b18
Thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-06-19 16:59:27 +0700

    showDataUser đã xong, chưa CSS

--- front_end/src/view/User/List_Apartment.jsx
+++ front_end/src/view/User/List_Apartment.jsx
@@ -1,12 +1,76 @@
-import React, { Component } from 'react';
-class List_Apartment extends Component {
-    render() {
-        return (
-            <div>
-                
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import '../../assets/style/List_Apartment.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function List_Apartment() {
+  const [shortTermApartments, setShortTermApartments] = useState([]);
+  const [longTermApartments, setLongTermApartments] = useState([]);
+
+  useEffect(() => {
+    // Gọi API để lấy dữ liệu căn hộ
+    fetch('http://localhost:8000/api/get-apartment')
+      .then(response => response.json())
+      .then(data => {
+        // Lọc và chỉ lấy dữ liệu cho các loại phòng ngắn hạn và dài hạn
+        const shortTerm = data.filter(apartment => apartment.type_room === 'Phòng ngắn hạn');
+        const longTerm = data.filter(apartment => apartment.type_room === 'Phòng dài hạn');
+        setShortTermApartments(shortTerm);
+        setLongTermApartments(longTerm);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className='show-apartment'>
+      <div className="apartment-list">
+        <h1>Short Term Apartments</h1>
+        {shortTermApartments.map(apartment => (
+          <div key={apartment.apartment_id} className="card">
+            <div className="image-gallery">
+              <Slider>
+                {apartment.apartment_image.map(image => (
+                  <div key={image.image_id}>
+                    <img src={image.image_path} alt="Apartment" />
+                  </div>
+                ))}
+              </Slider>
             </div>
-        );
-    }
+            <h2>{apartment.name}</h2>
+            <p>{apartment.description}</p>
+            <p>Price: {apartment.price}</p>
+            <p>Number of Rooms: {apartment.number_of_rooms}</p>
+            <p>Area: {apartment.area}</p>
+            <p>Address: {apartment.address}</p>
+            
+          </div>
+        ))}
+      </div>
+      <div className="apartment-list">
+        <h1>Long Term Apartments</h1>
+        {longTermApartments.map(apartment => (
+          <div key={apartment.apartment_id} className="card">
+            <div className="image-gallery">
+              <Slider>
+                {apartment.apartment_image.map(image => (
+                  <div key={image.image_id}>
+                    <img src={image.image_path} alt="Apartment" />
+                  </div>
+                ))}
+              </Slider>
+            </div>
+            <h2>{apartment.name}</h2>
+            <p>{apartment.description}</p>
+            <p>Price: {apartment.price}</p>
+            <p>Number of Rooms: {apartment.number_of_rooms}</p>
+            <p>Area: {apartment.area}</p>
+            <p>Address: {apartment.address}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default List_Apartment;
